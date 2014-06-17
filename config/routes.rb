07a74e5d763543bfc6fae612d380dae8d7b97fcb
gnazarkin <gleb.nazarkin@gmail.com>
2014-06-17 19:02:26 -0400

    Making pictures show

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Photogur::Application.routes.draw do
-  get 'pictures' => 'pictures#index'
+  root 'pictures#index'
+  resources :pictures
 end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -55,4 +56,3 @@ end
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
